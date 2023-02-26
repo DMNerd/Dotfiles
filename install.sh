0bf7a358d55d29b13bf92c6ea4988099c97083c6
Adam <7889445+DMNerd@users.noreply.github.com>
2023-02-26 22:47:29 +0100

    Refactor for the script

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
-stow -t $HOME/.config -v yabai
+#YABAI and SKHD
+#Check if stow is installed using homebrew
+brew list stow || brew install stow
+
+#Remove existing folder
+rm -rf $HOME/.config/yabai
+#Recreate it
+mkdir $HOME/.config/yabai 
+stow -t $HOME/.config/yabai -v yabai
+
+rm -rf $HOME/.config/skhd
+mkdir $HOME/.config/skhd
+stow -t $HOME/.config/skhd -v skhd
+
+#ZSH
+brew list pfetch || brew install pfetch
+brew list bpytop || brew install bpytop
+brew list lsd || brew install lsd
+
+stow -t $HOME -v zsh
