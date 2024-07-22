ed5b9c808496ce50fc160b64c6fd0791308ea8fb
Adam <7889445+DMNerd@users.noreply.github.com>
2024-07-22 12:42:33 +0200

    skhd config

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #NIX
 darwin-rebuild switch --flake ./nix-darwin
 
-#SKHD 
-stow -t $HOME/.config/skhd -D skhd
-stow -t $HOME/.config/skhd -v skhd
-
 #Raycast
 stow -t $HOME/.config/raycast -D raycast
 stow -t $HOME/.config/raycast -v raycast
