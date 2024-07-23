069af654d6cf95b4ee190a05e735dc658bf650a1
Adam <7889445+DMNerd@users.noreply.github.com>
2024-07-23 22:43:54 +0200

    move fonts to packages
    makes more sense than in system

--- nix-darwin/modules/packages.nix
+++ nix-darwin/modules/packages.nix
@@ -9,4 +9,26 @@
     skhd
     yabai
   ];
+  # Fonts
+  fonts = {
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
+      fira-code-symbols
+      # nerdfonts
+      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
+      (nerdfonts.override {
+        fonts = [
+          # symbols icon only
+          "NerdFontsSymbolsOnly"
+          # Characters
+          "SourceCodePro"
+          "Hack"
+          "Iosevka"
+          "Meslo"
+        ];
+      })
+    ];
+  };
 }
--- nix-darwin/modules/system.nix
+++ nix-darwin/modules/system.nix
@@ -193,27 +193,4 @@
 
   # Set your time zone.
   time.timeZone = "Europe/Prague";
-
-  # Fonts
-  fonts = {
-    packages = with pkgs; [
-      # icon fonts
-      material-design-icons
-      font-awesome
-      fira-code-symbols
-      # nerdfonts
-      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
-      (nerdfonts.override {
-        fonts = [
-          # symbols icon only
-          "NerdFontsSymbolsOnly"
-          # Characters
-          "SourceCodePro"
-          "Hack"
-          "Iosevka"
-          "Meslo"
-        ];
-      })
-    ];
-  };
 }
