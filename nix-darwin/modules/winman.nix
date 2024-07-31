2f0669d2abd908ba45e1dafac114801a55c4ca0e
Adam <7889445+DMNerd@users.noreply.github.com>
2024-07-31 10:45:34 +0200

    update, more yabai rules

--- nix-darwin/modules/winman.nix
+++ nix-darwin/modules/winman.nix
@@ -61,6 +61,7 @@
       yabai -m rule --add title='Preferences$' manage=off
       yabai -m rule --add title='^Archive Utility$' manage=off
       yabai -m rule --add label="Safari Floating" app="^Safari$" title="(General|Tabs|AutoFill|Passwords|Search|Security|Privacy|Websites|Profiles|Extensions|Advanced)" manage=off
+      yabai -m rule --add label="Mail Floating" app="^Mail$" title="(New Message|^Re.*$)" manage=off
       yabai -m rule --add title="(Copy|Bin|About This Mac|Info)" manage=off
       '';
   };
