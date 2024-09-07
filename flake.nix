19023dbef5923d9898e5e1582c88a1637489198d
Adam <7889445+DMNerd@users.noreply.github.com>
2024-09-07 11:20:29 +0200

    Revert flake to just darwin flake

--- flake.nix
+++ flake.nix
@@ -15,8 +15,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -24,12 +22,10 @@
   outputs = inputs @ {
     self,
     nix-darwin,
-    nixos-wsl,
     nixpkgs,
     home-manager,
   }: let
     systems = [
-      "x86_64-linux"
       "aarch64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
@@ -46,28 +42,6 @@
             useUserPackages = true;
             extraSpecialArgs = {inherit inputs;};
             users.dmnerd.imports = [./nix-darwin/home];
-            backupFileExtension = "bak";
-          };
-        }
-      ];
-    };
-    nixosConfigurations."WSL" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nix-wsl
-
-        nixos-wsl.nixosModules.default
-        {
-          system.stateVersion = "24.05";
-          wsl.enable = true;
-        }
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {inherit inputs;};
-            users.dmnerd.imports = [./nix-wsl/home];
             backupFileExtension = "bak";
           };
         }
--- nix-wsl/default.nix
+++
@@ -1,9 +0,0 @@
-{...}: {
-  # import sub modules
-  imports = [
-    # ./modules/wslcore.nix
-    # ./modules/system.nix
-    ./modules/wslcore.nix
-    ./modules/packages.nix
-  ];
-}
